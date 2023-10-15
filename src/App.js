8edd1771086758db6b24ab73e53a38ff01a53ea4
wasimreja <iwasimreja@gmail.com>
2023-10-15 18:45:19 +0530

    Fix navbar

--- src/App.js
+++ src/App.js
@@ -1,30 +1,46 @@
-import "./App.css";
-
+import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-
-import React from "react";
 import ImageCrypt from "./pages/ImageCrypt";
 import ImageDecrypt from "./pages/ImageDecrypt";
 import About from "./pages/About";
+import "./App.css";
+import "font-awesome/css/font-awesome.min.css";
 
 function App() {
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  const closeMobileMenu = () => {
+    setMobileMenuOpen(false);
+  };
+
   return (
     <Router>
       <nav className="navbar">
         <a href="/" className="BrandName">
           Stegano
         </a>
-        <ul className="nav">
-          <li>
-            <Link to="/">Encode</Link>
-          </li>
-          <li>
-            <Link to="/decode">Decode</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-        </ul>
+        <div className="MobileMenuToggle" onClick={toggleMobileMenu}>
+          {isMobileMenuOpen ? (
+            <i className="fa fa-times" />
+          ) : (
+            <i className="fa fa-bars" />
+          )}
+        </div>
+        <div className={`nav ${isMobileMenuOpen ? "open" : ""}`}>
+          <Link to="/" className="MenuItem" onClick={closeMobileMenu}>
+            Encode
+          </Link>
+          <Link to="/decode" className="MenuItem" onClick={closeMobileMenu}>
+            Decode
+          </Link>
+          <Link to="/about" className="MenuItem" onClick={closeMobileMenu}>
+            About
+          </Link>
+        </div>
       </nav>
 
       <Routes>
